02c6e6d4694cdfb250c69f7b98518327e30d81fa
gregorkschroeder <gregorkschroeder@gmail.com>
2018-06-28 15:15:11 -0700

    initial empty bca database build

---
+++ src/resources/build_bca.bat
@@ -0,0 +1,13 @@
+@echo off
+
+
+set script_path=
+set db_server=
+set db_name=
+rem db_path and log_path values must be enclosed in double quotes
+set db_path=
+set log_path=
+
+echo Creating %db_name% on %db_server% at %db_path%
+echo Log file at %log_path%
+sqlcmd -E -C -b -S %db_server% -i %script_path%create_bca_db.sql -v db_name=%db_name% db_path=%db_path% log_path=%log_path% || goto :EOF
