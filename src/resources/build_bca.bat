8de4d24eb64ea8e0d7f748be31d284bb579822d5
gregorkschroeder <gregorkschroeder@gmail.com>
2018-06-29 15:59:57 -0700

    added db_meta schema creation to the database build enabling easy metadata creation in the database

--- src/resources/build_bca.bat
+++ src/resources/build_bca.bat
@@ -11,3 +11,6 @@ set log_path=
 echo Creating %db_name% on %db_server% at %db_path%
 echo Log file at %log_path%
 sqlcmd -E -C -b -S %db_server% -i %script_path%create_bca_db.sql -v db_name=%db_name% db_path=%db_path% log_path=%log_path% || goto :EOF
+
+echo Creating db_meta schema and objects
+sqlcmd -E -C -b -S %db_server% -d %db_name% -i %script_path%db_meta.sql || goto :EOF
