4d867fb9e5c9047222c178d06d78d186a1bd2343
gregorkschroeder <gregorkschroeder@gmail.com>
2018-07-02 13:54:02 -0700

    created physical activity result function, updated auto ownership function comment

--- src/sql/programmability.sql
+++ src/sql/programmability.sql
@@ -22,7 +22,7 @@ CREATE FUNCTION [bca].[fn_auto_ownership]
 (
 	@scenario_id_base integer,
 	@scenario_id_build integer,
-	@auto_ownership_cost float
+	@auto_ownership_cost float -- bca auto ownership cost parameter
 )
 RETURNS @tbl_auto_ownership TABLE
 (
@@ -306,4 +306,140 @@ GO
 -- Add metadata for [bca].[fn_toll_cost]
 EXECUTE [db_meta].[add_xp] 'bca.fn_toll_cost', 'SUBSYSTEM', 'bca'
 EXECUTE [db_meta].[add_xp] 'bca.fn_toll_cost', 'MS_Description', 'function to return toll cost results for base and build scenarios'
+GO
+
+
+
+
+-- Create physical_activity table valued function
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[bca].[fn_physical_activity]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [bca].[fn_physical_activity]
+GO
+
+CREATE FUNCTION [bca].[fn_physical_activity]
+(
+	@scenario_id_base integer,
+	@scenario_id_build integer,
+	@activity_threshold float, -- bca minimum minutes to define person as physically active parameter
+	@activity_benefit float -- bca physically active person benefit parameter
+)
+RETURNS @tbl_physical_activity TABLE
+(
+	[base_active_persons] integer NOT NULL
+	,[build_active_persons] integer NOT NULL
+	,[diff_active_persons] integer NOT NULL
+	,[benefit_active_persons] float NOT NULL
+	,[diff_active_persons_coc] integer NOT NULL
+	,[benefit_active_persons_coc] float NOT NULL
+	,[diff_active_persons_senior] integer NOT NULL
+	,[benefit_active_persons_senior] float NOT NULL
+	,[diff_active_persons_minority] integer NOT NULL
+	,[benefit_active_persons_minority] float NOT NULL
+	,[diff_active_persons_low_income] integer NOT NULL
+	,[benefit_active_persons_low_income] float NOT NULL
+)
+AS
+
+-- ===========================================================================
+-- Author:		RSG and Gregor Schroeder
+-- Create date: 7/02/2018
+-- Description:	Translation and combination of the bca tool stored procedures
+-- for the new abm database listed below. Given two input scenario_id values,
+-- a threshold for minimum physical activity to define a person as active,
+-- and the benefit of a person being physically active then returns table of
+-- total physically active persons for the base and build scenarios by
+-- Community of Concern and each element that indicates a Community of Concern
+-- person (seniors, minorities, low income). Differences and benefits between
+-- two base and build scenarios are calculated as well.
+--	[dbo].[run_physical_activity_comparison]
+--	[dbo].[run_physical_activity_processor]
+--	[dbo].[run_physical_activity_summary]
+-- ===========================================================================
+BEGIN
+	with [active_persons] AS (
+		SELECT
+			[person_trip].[scenario_id]
+			,MAX(CASE WHEN [person].[age] >= 75 THEN 1 ELSE 0 END) AS [senior]
+			,MAX(CASE	WHEN [person].[race] IN ('Some Other Race Alone',
+													'Asian Alone',
+													'Black or African American Alone',
+													'Two or More Major Race Groups',
+													'Native Hawaiian and Other Pacific Islander Alone',
+													'American Indian and Alaska Native Tribes specified; or American Indian or Alaska Native, not specified and no other races')
+								OR [person].[hispanic] = 'Hispanic' THEN 1
+							ELSE 0 END) AS [minority]
+			,MAX(CASE WHEN [household].[poverty] <= 2 THEN 1 ELSE 0 END) AS [low_income]
+			,MAX([person].[weight_person]) AS [weight_person]
+		FROM
+			[fact].[person_trip]
+		INNER JOIN
+			[dimension].[person]
+		ON
+			[person_trip].[scenario_id] = [person].[scenario_id]
+			AND [person_trip].[person_id] = [person].[person_id]
+		INNER JOIN
+			[dimension].[household]
+		ON
+			[person_trip].[scenario_id] = [household].[scenario_id]
+			AND [person_trip].[household_id] = [household].[household_id]
+		WHERE
+			[person_trip].[scenario_id] IN (@scenario_id_base, @scenario_id_build)
+			AND [person].[scenario_id] IN (@scenario_id_base, @scenario_id_build)
+			AND [household].[scenario_id] IN (@scenario_id_base, @scenario_id_build)
+			AND [person].[weight_person] > 0
+			AND [household].[weight_household] > 0
+		GROUP BY
+			[person_trip].[scenario_id]
+			,[person_trip].[person_id]
+		HAVING
+			SUM([person_trip].[time_walk] + [person_trip].[time_bike]) > @activity_threshold),
+	[active_persons_summary] AS (
+		SELECT
+			[scenario_id]
+			,SUM([weight_person]) AS [active_persons]
+			,SUM(CASE WHEN [senior] = 1 OR [minority] = 1 OR [low_income] = 1 THEN [weight_person] ELSE 0 END) AS [active_persons_coc]
+			,SUM(CASE WHEN [senior] = 1 THEN [weight_person] ELSE 0 END) AS [active_persons_senior]
+			,SUM(CASE WHEN [minority] = 1 THEN [weight_person] ELSE 0 END) AS [active_persons_minority]
+			,SUM(CASE WHEN [low_income] = 1 THEN [weight_person] ELSE 0 END) AS [active_persons_low_income]
+		FROM
+			[active_persons]
+		GROUP BY
+			[scenario_id]),
+	[scenario_summary] AS (
+		SELECT
+			SUM(CASE WHEN [scenario_id] = @scenario_id_base THEN [active_persons] ELSE 0 END) AS [base_active_persons]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_build THEN [active_persons] ELSE 0 END) AS [build_active_persons]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_base THEN [active_persons_coc] ELSE 0 END) AS [base_active_persons_coc]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_build THEN [active_persons_coc] ELSE 0 END) AS [build_active_persons_coc]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_base THEN [active_persons_senior] ELSE 0 END) AS [base_active_persons_senior]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_build THEN [active_persons_senior] ELSE 0 END) AS [build_active_persons_senior]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_base THEN [active_persons_minority] ELSE 0 END) AS [base_active_persons_minority]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_build THEN [active_persons_minority] ELSE 0 END) AS [build_active_persons_minority]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_base THEN [active_persons_low_income] ELSE 0 END) AS [base_active_persons_low_income]
+			,SUM(CASE WHEN [scenario_id] = @scenario_id_build THEN [active_persons_low_income] ELSE 0 END) AS [build_active_persons_low_income]
+		FROM
+			[active_persons_summary])
+	INSERT INTO @tbl_physical_activity
+	SELECT
+		[base_active_persons]
+		,[build_active_persons]
+		,[build_active_persons] - [base_active_persons] AS [diff_active_persons]
+		,([build_active_persons] - [base_active_persons]) * @activity_benefit AS [benefit_active_persons]
+		,[build_active_persons_coc] - [base_active_persons_coc] AS [diff_active_persons_coc]
+		,([build_active_persons_coc] - [base_active_persons_coc]) * @activity_benefit AS [benefit_active_persons_coc]
+		,[build_active_persons_senior] - [base_active_persons_senior] AS [diff_active_persons_senior]
+		,([build_active_persons_senior] - [base_active_persons_senior]) * @activity_benefit AS [benefit_active_persons_senior]
+		,[build_active_persons_minority] - [base_active_persons_minority] AS [diff_active_persons_minority]
+		,([build_active_persons_minority] - [base_active_persons_minority]) * @activity_benefit AS [benefit_active_persons_minority]
+		,[build_active_persons_low_income] - [base_active_persons_low_income] AS [diff_active_persons_low_income]
+		,([build_active_persons_low_income] - [base_active_persons_low_income]) * @activity_benefit AS [benefit_active_persons_low_income]
+	FROM
+		[scenario_summary]
+	RETURN
+END
+GO
+
+-- Add metadata for [bca].[fn_physical_activity]
+EXECUTE [db_meta].[add_xp] 'bca.fn_physical_activity', 'SUBSYSTEM', 'bca'
+EXECUTE [db_meta].[add_xp] 'bca.fn_physical_activity', 'MS_Description', 'function to return person physical activity results for base and build scenarios'
 GO
